1ac110aedf4e75ef766b5bc3f16a2acf67812449
anon <75766435+anon1192@users.noreply.github.com>
2023-11-12 18:14:04 -0500

    updates

--- main.py
+++ main.py
@@ -7,30 +7,34 @@ from UserInterface import get_code, exit_program
 # Test ID, 48905
 id = input("Enter Drug Identification Number (DIN): ")
 
-print("What information do you want to receive on the Drug?")
+codes = ["DIF", "CIF", "AIN", "DFM", "PTY", "PST", "ROA", "DSC", "DST", "TCL"]
 
-code = get_code()
-while code == "error":
-    print ("Please re-enter a valid code, or press q to quit")
-    code = get_code()
+code_dict_title = {
+    "DIF": "Drug Product Information",
+    "CIF": "Company Information",
+    "AIN": "Active Ingredients",
+    "DFM": "Dosage Form",
+    "PTY": "Packaging Information",
+    "PST": "Pharmaceutical Standard",
+    "ROA": "Route of Administration",
+    "DSC": "Schedule Information",
+    "DST": "Status of Drug",
+    "TCL": "Therapeutic Class"
+}
 
-
-    codes = ["DIF", "CIF", "AI", "DFM", "PTY", "PST", "ROA", "DSC", "DST", "TCL"]
-
-
-print("")
-url = url_builder(code, id)
-
-if stats(url):
-    json_data = json_response(url)
-    dict = json_data.items()
-    for key, value in json_data.items():
-        if value == "":
-            value = "no information found"
-        print(f"{key}: {value}")
-else:
-    print("Error: Code Not found")
-
+for element in codes:
+    print(code_dict_title[element] + "\n")
+    url = url_builder(element, id)
+    if stats(url):
+        json_data = json_response(url)
+        dict = json_data.items()
+        for key, value in json_data.items():
+            if value == "":
+                value = "no information found"
+            print(f"{key}: {value}")
+        print("\n")
+    else:
+        print("*** Error: Code Not Found/Applicable ***")
 
 
 
